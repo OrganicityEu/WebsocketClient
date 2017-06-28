510b2283588b984576a5622881f281a6e850d082
Morten Skov Jørgensen <morten.skov@alexandra.dk>
2017-06-28 12:46:51 +0200

    Updated test example

--- src/websockets/test/VanillaTestClient.java
+++ src/websockets/test/VanillaTestClient.java
@@ -16,38 +16,12 @@ public class VanillaTestClient implements WebsocketCallback{
 	
 	public VanillaTestClient(){
 		now = System.currentTimeMillis();
-		
-		
-		
-		//running instance of Websocket middleware:
 		wsc= new WebsocketClientStomp(this, "ws://31.200.243.76:8090/orion");
-		
-		
-		
 		startLoop();
-		
-		
-		
-		Runtime.getRuntime().addShutdownHook(new Thread() {
-
-		    @Override
-		    public void run() {
-		        wsc.closeDown();
-		    }
-
-		});
 	}
-	int count = 0;
+	
 	private void startLoop(){
 		
-		//fetch access token
-		String clientId="XX";
-		String clientSecret = "XX";
-		String grantType = "client_credentials";
-		//TokenFetcher is only an example class of how to get the first token.
-		String token = new TokenFetcher(clientId, clientSecret, grantType).getToken();
-		
-
 		
 		while(true){
 			if(System.currentTimeMillis()>now+5000){
@@ -57,20 +31,20 @@ public class VanillaTestClient implements WebsocketCallback{
 					  attr[0] = "comments";
 					  String[] cond = new String[1];
 					  cond[0] = "comments";
-					  String entityId = "urn:oc:entity:experimenters:5a660d96-0ef7-42ca-9f6c-5dbb86d6aa20:58ab32f36f8b513746565c54:wsasset";
-					  //reference: https://fiware-orion.readthedocs.io/en/develop/user/walkthrough_apiv2/index.html#subscriptions
-					  //A subscription can be created, either as a naive implementation where all information about an entity if received or by utilizing the more advanced features
-					  //naive
-					  //OrionSubscription subscription = new OrionSubscription(entityId, token);
+					  String entityId = "<Your asset id here>";
+					  String token = "<Your token here>";
 					  
-					  //advanced
+					  //Reference: https://fiware-orion.readthedocs.io/en/develop/user/walkthrough_apiv2/index.html#subscriptions
+					  //Example of subscribing to certain asset conditions and attributes
 					  OrionSubscription subscription =  new OrionSubscription(cond, attr, entityId,token);
 					  
+					  //Example of subscribing to a certain asset without any specific conditions
+					  //OrionSubscription subscription = new OrionSubscription(entityId, token);
 					  
 					  System.out.println("trying to set subscrition");
 					  hasSubscription = wsc.registerSubscription(subscription);
 					  if(hasSubscription){
-						  System.out.println("subscription request sent");
+						  System.out.println("subscription set");
 					  }  
 				  }else{
 					  List<String> subscriptions = wsc.getSubscriptions();
@@ -95,12 +69,6 @@ public class VanillaTestClient implements WebsocketCallback{
 	public void messageReceived(String mesg) {
 		System.out.println("Received message in Vanilla: "+mesg);
 	}
-	
-	@Override
-	public void finalize(){
-		System.out.println("test exit");
-	}
-	
 	
 	
 	public static void main(String[] args){
